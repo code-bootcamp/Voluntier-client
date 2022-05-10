389f2aaac32d0eb5360d523e74e8b55be55df16e
ChoiChangSeo <navida31245@gmail.com>
2022-05-10 20:47:37 +0900

    ✨Update SignUp

---
+++ voluntier/src/components/units/Signup/SignUpContainer.tsx
@@ -0,0 +1,37 @@
+import SignUpUI from "./SignUpPresenter";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+import * as yup from "yup"
+
+const schema = yup.object({
+    email: yup.string().email("이메일의 형식이 올바르지 않습니다.").required("이메일은 필수 입력사항 입니다."),
+    name: yup.string().required("이름은 필수 입력사항입니다."),
+    password: yup.string().matches( 
+        /^(?=.[A-Za-z])(?=.\d)(?=.[$@$!%#?&])[A-Za-z\d$@$!%#?&]{1,8}$/,
+        "비밀번호는 영문, 숫자, 특수문자를 포함한 8자리 이내 문자열이어야 합니다"),
+    PasswordCheck : yup.string().oneOf([yup.ref('password'), null], "패스워드와 일치하지 않습니다."),
+    PhoneNumber : yup.number().typeError("핸드폰 번호는 숫자로만 입력해주세요.").required("핸드폰 번호는 필수 입려사항 입니다."),
+    PhoneNumberCheck : yup.number().typeError("핸드폰 번호 인증은 숫자로만 입력해주세요").required("핸드폰 번호 인증은 필수입니다.")
+})
+
+// interface IFormValues {
+//     email : string
+//     name: string
+//     password : string
+//     checkPassword : string
+// }
+
+export default function SignUp(){
+
+    const {register , handleSubmit, formState} = useForm({
+        resolver : yupResolver(schema)
+    })
+
+    return(
+        <SignUpUI 
+        register={register} 
+        handleSubmit={handleSubmit}
+        formState={formState}
+        />
+    )
+}
