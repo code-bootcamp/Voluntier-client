c3bd365d0296f18dbdd51c83ae1739b97d7606cb
ChoiChangSeo <navida31245@gmail.com>
2022-05-10 18:05:07 +0900

    ✨globalStlyes_RecoilRoot

--- voluntier/pages/_app.tsx
+++ voluntier/pages/_app.tsx
@@ -1,7 +1,21 @@
 import '../styles/globals.css'
+import 'antd/dist/antd.css'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { Global } from "@emotion/react";
+import { globalStyles } from "../src/commons/styles/globalStyles";
+import { AppProps } from "next/app";
+import { RecoilRoot } from 'recoil';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }:AppProps) {
+  return (
+  <>
+  <RecoilRoot>
+    <Global styles={globalStyles}/>
+    <Component {...pageProps} />
+  </RecoilRoot>
+  </>
+  )
 }
 
 export default MyApp
---
+++ voluntier/src/commons/styles/globalStyles.ts
@@ -0,0 +1,13 @@
+import { css } from "@emotion/react";
+
+export const globalStyles = css`
+html, body {
+  width:100%;
+}
+
+  * {
+    margin: 0;
+    box-sizing: border-box;
+    font-size: 10px;
+  }
+`;
