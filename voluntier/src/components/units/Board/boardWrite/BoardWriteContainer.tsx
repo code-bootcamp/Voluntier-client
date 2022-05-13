67661b68304f87709a2027391f33e13fc01c967f
ChoiChangSeo <navida31245@gmail.com>
2022-05-13 14:41:50 +0900

    ✨CreateKaKaoMap

--- voluntier/src/components/units/Board/boardWrite/BoardWriteContainer.tsx
+++ voluntier/src/components/units/Board/boardWrite/BoardWriteContainer.tsx
@@ -1,5 +1,33 @@
+import { useState } from "react";
 import BoardWriteUI from "./boardWritePresenter";
 
 export default function BoardWrite() {
-  return <BoardWriteUI />;
+const [isModalVisible, setIsModalVisible] = useState(false);
+const [address,setAddress] = useState("")
+
+const handleComplete = (data: any) => {
+  setIsModalVisible((prev) => !prev);
+  console.log(data.address)
+  setAddress(data.address);
+};
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+  return <BoardWriteUI
+        address={address}
+        handleComplete={handleComplete} 
+        showModal={showModal} 
+        handleOk={handleOk}
+        handleCancel={handleCancel}
+        isModalVisible={isModalVisible}
+  />;
 }
