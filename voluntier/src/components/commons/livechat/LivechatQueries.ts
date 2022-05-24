7e92e4123cf481c138f64a0a4b3e762bb9a9675a
kwonkabi <seohyung0929@gmail.com>
2022-05-24 20:29:28 +0900

    🎉Update Livechat

--- voluntier/src/components/commons/livechat/LivechatQueries.ts
+++ voluntier/src/components/commons/livechat/LivechatQueries.ts
@@ -1,23 +1,14 @@
-// import { gql } from "@apollo/client";
+import { gql } from "@apollo/client";
 
-// export const FETCH_LOGIN_USER = gql`
-//   query fetchLoginUser {
-//     fetchLoginUser {
-//       id
-//       name
-//     }
-//   }
-// `;
-
-// export const FETCH_BOARD = gql`
-//   query fetchBoard($boardId: String!) {
-//     fetchBoard(boardId: $boardId) {
-//       user {
-//         id
-//         isAdmin
-//         name
-//       }
-//       centerName
-//     }
-//   }
-// `;
+export const FETCH_CHAT_HISTORY = gql`
+  query fetchChatHistory($boardId: String!) {
+    fetchChatHistory(boardId: $boardId) {
+      id
+      user {
+        id
+        name
+      }
+      message
+    }
+  }
+`;
