126ac53400a4fbb025820466487867a791a56f6b
ChoiChangSeo <navida31245@gmail.com>
2022-05-30 07:21:57 +0900

    aaa

--- voluntier/src/components/commons/kakaomapGeolocation/index.tsx
+++ voluntier/src/components/commons/kakaomapGeolocation/index.tsx
@@ -73,7 +73,7 @@ export default function KakaomapGeolocation() {
           infowindow.open(map, marker);
         };
       };
-a
+
       const makeOutListener = (infowindow: any) => {
         return function () {
           infowindow.close();
