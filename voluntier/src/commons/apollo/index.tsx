e65b91d576bde142bc53571e4d79a72be69e8ff9
ChoiChangSeo <navida31245@gmail.com>
2022-08-16 17:56:33 +0900

    refreshToken Fix

--- voluntier/src/commons/apollo/index.tsx
+++ voluntier/src/commons/apollo/index.tsx
@@ -1,52 +1,58 @@
-import { ApolloClient, ApolloLink, InMemoryCache, ApolloProvider } from "@apollo/client";
+import {
+  ApolloClient,
+  ApolloLink,
+  InMemoryCache,
+  ApolloProvider,
+  fromPromise,
+} from "@apollo/client";
 import { createUploadLink } from "apollo-upload-client";
 import { useRecoilState } from "recoil";
 import { accessTokenState } from "../store";
-import { ReactNode, useEffect} from "react";
+import { ReactNode, useEffect } from "react";
 import { getAccessToken } from "../libraries/getAccessToken";
 import { onError } from "@apollo/client/link/error";
 
-
-interface IApolloSetting{
-    children: ReactNode
+interface IApolloSetting {
+  children: ReactNode;
 }
 
-export default function ApolloSetting(props:IApolloSetting){
-    const [accessToken,setAccessToken] = useRecoilState(accessTokenState);
+export default function ApolloSetting(props: IApolloSetting) {
+  const [accessToken, setAccessToken] = useRecoilState(accessTokenState);
 
-    useEffect(() => {
-      getAccessToken().then((newAccessToken) => {
-        setAccessToken(newAccessToken);
-      });
-      }, []);
-      
-      const errorLink = onError(({ graphQLErrors, operation, forward }) => {
-        if (graphQLErrors) {
-          for (const err of graphQLErrors) {
-            if (err.extensions.code === "UNAUTHENTICATED") {
-              getAccessToken().then((newAccessToken) => {
-                setAccessToken(newAccessToken);
-                operation.setContext({
-                  headers: {
-                    ...operation.getContext().headers,
-                    Authorization: `Bearer ${newAccessToken}`,
-                  } 
-                });
-                return forward(operation);
+  useEffect(() => {
+    getAccessToken().then((newAccessToken) => {
+      setAccessToken(newAccessToken);
+    });
+  }, []);
+
+  const errorLink = onError(({ graphQLErrors, operation, forward }) => {
+    if (graphQLErrors) {
+      for (const err of graphQLErrors) {
+        if (err.extensions.code === "UNAUTHENTICATED") {
+          return fromPromise(
+            getAccessToken().then((newAccessToken) => {
+              setAccessToken(newAccessToken);
+              operation.setContext({
+                headers: {
+                  ...operation.getContext().headers,
+                  Authorization: `Bearer ${newAccessToken}`,
+                },
               });
-            }
-          }
+            })
+          ).flatMap(() => forward(operation));
         }
-      });
+      }
+    }
+  });
 
-    const uploadLink = createUploadLink({
-        uri: "https://backendapi.voluntier.site/graphql",
-        headers:{authorization : `Bearer ${accessToken}`},
-        credentials: "include",
-      });
-      const client = new ApolloClient({
-        link : ApolloLink.from([errorLink,uploadLink]),
-        cache: new InMemoryCache(),
-      });
-    return(<ApolloProvider client={client}>{props.children}</ApolloProvider>)
+  const uploadLink = createUploadLink({
+    uri: "https://backendapi.voluntier.site/graphql",
+    headers: { authorization: `Bearer ${accessToken}` },
+    credentials: "include",
+  });
+  const client = new ApolloClient({
+    link: ApolloLink.from([errorLink, uploadLink]),
+    cache: new InMemoryCache(),
+  });
+  return <ApolloProvider client={client}>{props.children}</ApolloProvider>;
 }
